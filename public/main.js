c99cff2cda5eec6b3eee4231974fa533818b38f2
Evgi100 <evgeni.shapira@gmail.com>
2017-09-26 19:57:30 +0300

    Add image slideshow as bacground

--- public/main.js
+++ public/main.js
@@ -163,3 +163,18 @@ $('.view-cart').on('click', function() {
     shoppingCart = $('.shopping-cart');
     shoppingCart.toggle();
 });
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 7000); // Change image every 2 seconds
+}
